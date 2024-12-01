89d3ee89b89872e8bcbf2f4b8d3e6531c914206d
zhekabaila <zhekabaila@gmail.com>
2024-12-01 16:53:07 +0700

    style: change svg to jpg

--- src/constant/teams.ts
+++ src/constant/teams.ts
@@ -3,7 +3,7 @@ export const teams = [
     name: 'Rakha Aji Mulia',
     npm: '247006111075',
     instagram: 'ryuzaki_rakha',
-    img: '/images/members/rakha.svg',
+    img: '/images/members/rakha.jpg',
     description:
       'Anggota kelompok yang berfokus mengecek eror pada kode dan meng evaluasi materi dan desain yang telah di buat kelompok.',
   },
@@ -11,21 +11,21 @@ export const teams = [
     name: 'Muhammad Fawwazul Haq',
     npm: '247006111088',
     instagram: 'fawwazulhqq',
-    img: '/images/members/fawwaz.svg',
+    img: '/images/members/fawwaz.jpg',
     description: 'Anggota kelompok yang berfokus membuat desain web.',
   },
   {
     name: 'Zheka Baila Arkan',
     npm: '247006111152',
     instagram: 'zhekabailaa',
-    img: '/images/members/zheka.svg',
+    img: '/images/members/zheka.jpg',
     description: 'Anggota kelompok yang berfokus sebagai pengembang web.',
   },
   {
     name: 'Muhammad Alif Akhdan Tsani',
     npm: '247006111166',
     instagram: 'alifakhdann',
-    img: '/images/members/alif.svg',
+    img: '/images/members/alif.jpg',
     description:
       'Anggota kelompok yang berfokus dalam mencari materi yang akan ada di web dan sebagai pemateri dalam web. ',
   },
